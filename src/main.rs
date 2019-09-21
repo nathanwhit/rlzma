21a81fc85166cf59e22939f9f6f9ab69f472ec11
nathanwhit <nathan.whitaker01@gmail.com>
2019-09-21 16:13:41 -0400

    Print out entire error chain info in main

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,18 @@ extern crate lzma_rs;
 use lzma_rs::lzma::decoder::{LZMADecoder};
 use std::fs::{File};
 use std::path::Path;
-use error_chain::quick_main;
-use lzma_rs::errors::Result;
+use error_chain::ChainedError;
+use std::process::exit;
+// use lzma_rs::errors::Result;
 
-quick_main!(run);
-
-fn run() -> Result<()> {
+fn main() {
     let in_path = Path::new("simple.txt.lzma");
     let out_path = Path::new("simple_decoded.txt");
-    let out_file = File::create(out_path)?;
-    let in_file = File::open(in_path)?;
+    let out_file = File::create(out_path).expect("Failed to create output file");
+    let in_file = File::open(in_path).expect("Failed to open input file");
     let mut decoder = LZMADecoder::new(in_file, out_file);
-    decoder.decode()?;
-    Ok(())
+    decoder.decode().unwrap_or_else(|e| {
+        eprintln!("{}", e.display_chain().to_string());
+        exit(1);
+    });
 }
